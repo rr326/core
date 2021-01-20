b435d486514a24fd27c25c9b26e79616493aeab7
Ross Rosen <rrosen326@gmail.com>
2021-01-20 20:32:41 +0000

    (split) Deleted old INBOUND websocket code.
    Now relies on being called as a service.
    
    NOTE - there IS something broken - I get the following warning
    which does not go away. Must investigate:
    
    Home Assistant is starting, not everything will be available until it is finished.

--- __init__.py
+++ __init__.py
@@ -12,13 +12,11 @@ import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN, WS_TYPE_MESSAGE
 import asyncio
 from . import test
-from . import ws_in
 from .services import setup_services
 
 
 async def async_setup(hass: HomeAssistant, config: dict):
     await setup_services(hass, config)
-    await ws_in.register_handlers(hass)
 
     hass.async_add_job(test.send_every_five(hass))
 
